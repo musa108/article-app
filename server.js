26fd64c7bd5077c348b0808b0147ef31f842e26d
unknown <nmanagi23@gmail.com>
2023-08-10 11:39:18 +0100

    update: first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const articleRouter = require('./routes/articleS');
+const Article = require('./models/article');
+const methodOverride = require('method-override')
+const app = express();
+
+mongoose.connect('mongodb://127.0.0.1/blog', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false}))
+app.use(methodOverride('_method'))
+
+app.get('/', async (req,res) =>{
+const articles = await Article.find().sort({
+    CreatedAt: 'desc'
+})
+    res.render('articles/index',{articles: articles})
+})
+
+app.use('/articles',articleRouter)
+app.listen(5000)
